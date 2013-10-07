498b28f583cb1b1a1956ce3a2ebb1c89bbcf527b
gemfarmer <brianhedberg@Brians-MacBook-Pro.local>
2013-10-07 00:27:43 -0600

    spread one cell complete

--- main.js
+++ main.js
@@ -1,80 +1,103 @@
 $(function() {
 
 
-$( "li" ).each(function( ) {
-  console.log( index + ": " + $( this ).text() );
-});
 
-// var setIndex = function(element){
+	//add index-n to link buttons
 
+	$('a').each(function(index){
+		$(this).attr('data-index', index);
+	});
 
-// 	for (var i = 0; i < element.length; i++) {
-// 		element[i].addClass('data-index' +i)
-// 	};
-// }
-
-// setIndex($('ul'));
-
-$('#lake').on('click', function(){
-	$('#campfire').removeClass("selected")
-	$(this).addClass("selected")
-	
-	preventFire();
-	
-});
-$('#campfire').on('click', function(){
-	$('#lake').removeClass("selected")
-	$(this).addClass("selected")
-	
-	spreadFire();
-	setFireInterval();
-});
-
-
-var spreadFire = function(){
-	console.log("fire")
-	$('.btn-default').off('click')
-	$('.btn-default').on('click', function(){
-		($(this).hasClass("btn-info"))
-			? console.log("break")
-			: $(this).addClass("btn-warning")
-	})
-
-}
-var preventFire = function(){
-	console.log("water")
-	$('.btn-default').off('click')
-	$('.btn-default').on('click', function(){
-		($(this).hasClass("btn-warning") || $(this).hasClass("btn-danger")) 
-			? console.log("break")
-			: $(this).addClass("btn-info")
-	
-	})
-
-}
-
-
-
-var setFireInterval = function(){
-	setInterval(function(){
+	$('#lake').on('click', function(){
+		$('#campfire').removeClass("selected")
+		$(this).addClass("selected")
 		
-		$('.btn-default').hasClass('btn-warning')
-			? $('.btn-warning').addClass("btn-danger")
-			: console.log("no fire to spread")
+		preventFire();
+		
+	});
+	$('#campfire').on('click', function(){
+		$('#lake').removeClass("selected")
+		$(this).addClass("selected")
+		
+		startFire();
+		setFireInterval();
+	});
 
-		var neighbor = $('.btn-default').parents('li').next().children('btn-default');
 
-		console.log(neighbor)
-		if (neighbor.hasClass('btn-info')){
-			console.log("fire cannot breach water");
-		}
-		else {
-			neighbor.addClass('btn-danger');
-		}
-		neighbor.css("background-color", "black")
+
+
+	var startFire = function(){
+		// console.log("fire")
+		$('.btn-default').off('click')
+		$('.btn-default').on('click', function(){
+			($(this).hasClass("btn-info"))
+				? console.log("break")
+				: $(this).addClass("btn-warning").addClass("fireup")
 			
+		})
+
+
+	}
+	var preventFire = function(){
+		console.log("water")
+		$('.btn-default').off('click')
+		$('.btn-default').on('click', function(){
+			($(this).hasClass("btn-warning") || $(this).hasClass("btn-danger")) 
+				? console.log("break")
+				: $(this).addClass("btn-info")
+		
+		})
+
+	}
+	var setFireInterval = function(){
+		setTimeout(function(){
+			
+			$('.btn-default').hasClass('btn-warning')
+				? $('.btn-warning').addClass("btn-danger")
+				: console.log("no fire to spread")
+			spreadFire();
+		}, 1500);
+	}
+	var findIndexNext = function(){
+
+		var nextClassItem = $('.fireup').attr("data-index")
+		console.log("firestring", typeof(nextClassItem), nextClassItem)
+		nextClassItem = Number(nextClassItem)
+		console.log("firenumber", typeof(nextClassItem), nextClassItem)
+		var nextNeighbor = nextClassItem+1;
+		console.log("next", nextNeighbor)
 	
-	}, 1500);
-}
+		
+		return nextNeighbor;
+	}
+	var findIndexPast = function(){
+		var pastClassItem = $('.fireup').attr("data-index")
+		console.log("firestring", typeof(pastClassItem), pastClassItem)
+		pastClassItem = Number(pastClassItem)
+		console.log("firenumber", typeof(pastClassItem), pastClassItem)
+		var pastNeighbor = pastClassItem-1;
+		console.log("past", pastNeighbor)
+		return pastNeighbor;
+	}
+
+	var spreadFire = function(){
+		
+
+
+		var pastIndex = findIndexPast();
+		var pastIndexString = pastIndex.toString();
+		console.log("pastIndexstring", typeof(pastIndexString), pastIndexString)
+		var nextIndex = findIndexNext();
+		var nextIndexString = nextIndex.toString();
+	
+
+		$('[data-index='+ pastIndexString +']').addClass('btn-warning').addClass('.fireup')
+			console.log("came this far")
+			
+		$('[data-index='+ nextIndexString +']').addClass('btn-warning').addClass('.fireup')
+	}
+
+
+	
 
 });
